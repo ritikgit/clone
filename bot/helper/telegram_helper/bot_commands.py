7d57dec8b9b8f9219844cf6fff55ad07f290fde1
ritikgit <63956562+ritikgit@users.noreply.github.com>
2020-05-24 17:43:11 +0530

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -1,8 +1,8 @@
 class _BotCommands:
     def __init__(self):
         self.StartCommand = 'start'
-        self.MirrorCommand = 'td13'
-        self.TarMirrorCommand = 'td13tar'
+        self.MirrorCommand = 'td15'
+        self.TarMirrorCommand = 'td15tar'
         self.CancelMirror = 'cancel9'
         self.CancelAllCommand = 'cancelall'
         self.ListCommand = 'list'
@@ -14,8 +14,8 @@ class _BotCommands:
         self.StatsCommand = 'stats'
         self.HelpCommand = 'help'
         self.LogCommand = 'log'
-        self.CloneCommand = "td13clone"
-        self.WatchCommand = 'td13watch'
-        self.TarWatchCommand = 'td13tarwatch'
+        self.CloneCommand = "td15clone"
+        self.WatchCommand = 'td15watch'
+        self.TarWatchCommand = 'td15tarwatch'
 
 BotCommands = _BotCommands()
